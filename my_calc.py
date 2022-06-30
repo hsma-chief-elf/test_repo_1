b5f4d24aec8b5a6578939ee51a09be73b27e7451
hsma_master <d.chalk@exeter.ac.uk>
2022-06-30 09:51:17 +0100

    INIT: add my_calc.py

---
+++ my_calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 09:43:30 2022
+
+@author: dan
+"""
+
+
+def add_numbers(num_1, num_2):
+    result = num_1 + num_2
+    
+    return result
+
